5ccb0fb9af6430e0925e68ce6c75abb4f7830cfb
Antoan Petrov <antoan.petrov@delasport.com>
2023-01-07 15:22:33 +0200

    delete message method

--- components/Car/MessageCard.vue
+++ components/Car/MessageCard.vue
@@ -1,20 +1,24 @@
 <script setup>
 
     const props = defineProps({
-        message: Object
+        message: Object,
+        id: String
     })
+
+    const emits = defineEmits(['deleteMessageClick']);
 
 </script>
 
 <template>
   <div class="even:bg-gray-100 p-4">
-    <h1 class="font-fold text-2xl">{{ message.name }}</h1>
-    <p class="text-gray-700">
+      <h1 class="font-fold text-2xl">{{ message.name }}</h1>
+      <p class="text-gray-700">
       {{message.message}}
     </p>
-    <div class="flex items-center mt-4">
-      <p class="text-green-700 mr-10">{{ message.email }}</p>
-      <p class="text-blue-500">{{ message.phone }}</p>
-    </div>
+      <div class="flex items-center mt-4">
+          <p class="text-green-700 mr-10">{{ message.email }}</p>
+          <p class="text-blue-500">{{ message.phone }}</p>
+      </div>
+      <p class="text-red-400 cursor-pointer mt-5"  @click="emits('deleteMessageClick', message.id)">Delete</p>
   </div>
 </template>
--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -5,11 +5,22 @@ definePageMeta({
 
 const route = useRoute();
 
-const { data: messages } = useFetch(`/api/car/listings/${route.params.id}/message`);
+const { data: messages, refresh } = useFetch(`/api/car/listings/${route.params.id}/message`);
+
+const handleDelete = async (id) => {
+    let confirmMessage = confirm("Are you sure to delete this?");
+
+    if (confirmMessage) {
+        await $fetch(`/api/car/listings/view/${id}`, {
+            method: "DELETE"
+        });
+        refresh();
+    }
+};
 </script>
 
 <template>
   <div class="rounded shadow mt-20">
-    <CarMessageCard v-for="message in messages" :key="message.id" :message="message"/>
+    <CarMessageCard v-for="message in messages" :key="message.id" :message="message" :id="message.id" @delete-message-click="handleDelete(message.id)"/>
   </div>
 </template>
