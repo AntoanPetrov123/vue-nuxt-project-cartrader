11b4693082b914c0cc206563f3d703a297995f17
Antoan Petrov <antoan.petrov@delasport.com>
2023-01-07 10:51:36 +0200

    refactor getting car by prisma

--- server/api/cars/[city].get.js
+++ server/api/cars/[city].get.js
@@ -1,31 +1,38 @@
-import cars from "@/data/cars.json";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export default defineEventHandler((event) => {
     //filter listed cars by criteria
     const { city } = event.context.params;
     const {make, minPrice, maxPrice} = getQuery(event)
-    let filteredCars = cars;
 
-    filteredCars = filteredCars.filter(car => {
-        return car.city.toLocaleLowerCase() === city.toLocaleLowerCase();
+    //create filter for sorting cars
+    // by city(required),
+    // by make(optional),
+    // by price(optional)
+    const filters = {
+        city: city.toLocaleLowerCase()
+    }
+
+    if (make) {
+        filters.make = make;
+    }
+
+    //if any price set create an object in filters
+    if (minPrice || maxPrice) {
+        filters.price = {};
+    }
+
+    if (minPrice) {
+        filters.price.gte = parseInt(minPrice);
+    }
+
+    if (maxPrice) {
+        filters.price.lte = parseInt(maxPrice);
+    }
+
+    return prisma.car.findMany({
+        where: filters
     });
-
-    if(make) {
-        filteredCars = filteredCars.filter(car => {
-            return car.make.toLocaleLowerCase() === make.toLocaleLowerCase();
-        });
-    }
-
-    if(minPrice) {
-        filteredCars = filteredCars.filter(car => {
-            return +car.price >= +minPrice;
-        });
-    }
-    if(maxPrice) {
-        filteredCars = filteredCars.filter(car => {
-            return +car.price <= +maxPrice;
-        });
-    }
-
-    return filteredCars;
 });
